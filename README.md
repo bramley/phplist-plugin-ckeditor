1a54ddee923d38e36fcfad598b1d160babefbb5c
Duncan Cameron <phplist@dcameron.me.uk>
2013-04-11 20:50:43 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,65 @@
+# CKEditor Plugin #
+
+## Description ##
+This plugin provides CKEditor 4.1 for editing messages and templates within phplist. 
+
+It also integrates the KCFinder file manager to provide file upload and selection.
+## Compatibility ###
+
+CKEditor and KCFinder are compatible with all the major browsers, see the CKEditor site <http://ckeditor.com/about>
+and the KCFinder site <http://kcfinder.sunhater.com/>
+
+## Installation ##
+
+### Set the plugin directory ###
+The default plugin directory is `plugins` within the admin directory.
+
+You can use a directory outside of the web root by changing the definition of `PLUGIN_ROOTDIR` in config.php.
+The benefit of this is that plugins will not be affected when you upgrade phplist.
+### Install through phplist ###
+Install on the Plugins page (menu Config > Plugins) using the package URL `https://github.com/bramley/phplist-ckeditorplugin/archive/master.zip`.
+
+### Install manually ###
+Download the plugin zip file from <https://github.com/bramley/phplist-ckeditorplugin/archive/master.zip>
+
+Expand the zip file, then copy the contents of the plugins directory to your phplist plugins directory.
+This should contain
+
+* the file CKEditorPlugin.php
+* the directory CKEditorPlugin
+### Location of the CKEditor and KCFinder directories###
+The CKEditor and KCFinder directories must be within the web root. 
+
+If you have the default plugin location then the plugin will use the correct paths automatically.
+
+But if your plugin directory is outside of the web root then you must move or copy the `ckeditor` and `kcfinder` directories from
+the plugin's directory to somewhere within the web root.  
+Then use the Settings page (menu Config > Settings) to specify the path to each directory.
+In the Composition Settings section enter
+
+* the path to CKeditor
+* the path to KCFinder 
+
+Similarly, if you already use CKEditor on your web site then you can use that version by specifying the path on the Settings page.
+## Configuration ##
+The width and height of the editor window can be specified on the Settings page.
+
+The location of a directory where KCFinder can store uploaded images can be specified on the Settings page. The location entered here 
+will over-ride the UPLOADIMAGES_DIR value, if set, in config.php.
+Note that KCFinder will create two subdirectories, images and .thumbs, within the specified directory to store the full-size and thumbnail images.
+
+## Custom configuration ##
+Other settings for the editor can be placed in a custom configuration file. This file needs to be within the web root and its
+location specified on the Settings page.
+A sample custom configuration file is provided which can be used as the basis for your own settings.
+
+See <http://docs.ckeditor.com/#!/api/CKEDITOR.config> for how to specify configuration settings.
+## Styles ##
+
+Style definitions can be specified in the custom configuration file, and they will then appear in the Styles drop-down list when editing a message.
+Additionally, a CSS stylesheet file can be parsed to provide the style definitions.
+See <http://docs.ckeditor.com/#!/guide/dev_styles>
+## Version history ##
+
+    version     Description
+    2013-04-11  Initial version for phplist 2.11.x releases
---
+++ plugins/CKEditorPlugin.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * CKEditorPlugin for phplist
+ * 
+ * This file is a part of CKEditorPlugin.
+ *
+ * This plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @category  phplist
+ * @package   CKEditorPlugin
+ * @author    Duncan Cameron
+ * @copyright 2013 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ */
+class CKEditorPlugin extends phplistPlugin
+{
+	const VERSION_FILE = 'version.txt';
+	const CODE_DIR = 'CKEditorPlugin/';
+    /*
+     *  Inherited variables
+     */
+    public $name = 'CKEditor plugin';
+    public $editorProvider = true;
+    public $authors = 'Duncan Cameron';
+    public $enabled = 1;
+
+    function __construct()
+    {
+        $this->coderoot = dirname(__FILE__) . self::CODE_DIR;
+		$this->version = (is_file($f = $this->coderoot . self::VERSION_FILE))
+			? file_get_contents($f)
+            : '';
+        $this->settings = array(
+            'ckeditor_width' => array (
+              'value' => 600,
+              'description' => 'Width in px of CKeditor Area',
+              'type' => 'integer',
+              'allowempty' => 0,
+              'min' => 100,
+              'max' => 800,
+              'category'=> 'composition',
+            ),
+            'ckeditor_height' => array (
+              'value' => 600,
+              'description' => 'Height in px of CKeditor Area',
+              'type' => 'integer',
+              'allowempty' => 0,
+              'min' => 100,
+              'max' => 800,
+              'category'=> 'composition',
+            ),
+            'ckeditor_path' => array (
+              'value' => PLUGIN_ROOTDIR . '/' . self::CODE_DIR . 'ckeditor',
+              'description' => 'path to CKeditor',
+              'type' => 'text',
+              'allowempty' => 0,
+              'category'=> 'composition',
+            ),
+            'ckeditor_config_path' => array (
+              'value' => '',
+              'description' => 'path to CKeditor custom configuration file',
+              'type' => 'text',
+              'allowempty' => 1,
+              'category'=> 'composition',
+            ),
+            'kcfinder_path' => array (
+              'value' =>  PLUGIN_ROOTDIR . '/' . self::CODE_DIR . 'kcfinder',
+              'description' => 'path to KCFinder',
+              'type' => 'text',
+              'allowempty' => 0,
+              'category'=> 'composition',
+            ),
+            'kcfinder_upload_path' => array (
+              'value' => '',
+              'description' => 'path to file upload directory (overrides UPLOADIMAGES_DIR in config.php)',
+              'type' => 'text',
+              'allowempty' => 1,
+              'category'=> 'composition',
+            ),
+        );
+        parent::__construct();
+    }
+
+    function adminmenu()
+    {
+        return array();
+    }
+  
+    function editor($fieldname, $content)
+    {
+        $kcFinderConfig = array(
+            'disabled' => false
+        );
+        $upload = getConfig('upload_path');
+
+        if ($upload != '' || (defined('UPLOADIMAGES_DIR') && (($upload = UPLOADIMAGES_DIR) != ''))) {
+            $upload = ltrim($upload, '/');
+            $kcFinderConfig['uploadURL'] = "http://{$_SERVER['SERVER_NAME']}/$upload";
+        }
+        $_SESSION['KCFINDER'] = $kcFinderConfig;
+
+        $content = htmlspecialchars($content);
+        $kcPath = htmlspecialchars(rtrim(getConfig('kcfinder_path'), '/'));
+        $path = htmlspecialchars(rtrim(getConfig('ckeditor_path'), '/'));
+        $ckConfigPath = htmlspecialchars(rtrim(getConfig('ckeditor_config_path'), '/'));
+        $customConfig = $ckConfigPath ? "customConfig: '$ckConfigPath'," : '';
+
+        if ($fieldname == 'footer') {
+            $width = getConfig('ckeditor_width');
+            $height = 100;
+        } else {
+            $width = getConfig('ckeditor_width');
+            $height = getConfig('ckeditor_height');
+        }
+        $html = <<<END
+<script type="text/javascript" src="$path/ckeditor.js"></script>
+<textarea name="$fieldname">$content</textarea>
+<script>
+CKEDITOR.replace('$fieldname', {
+    $customConfig
+    width: $width,
+    height: $height,
+    filebrowserBrowseUrl: '$kcPath/browse.php?type=files',
+    filebrowserImageBrowseUrl: '$kcPath/browse.php?type=images',
+    filebrowserFlashBrowseUrl: '$kcPath/browse.php?type=flash',
+    filebrowserUploadUrl: '$kcPath/upload.php?type=files',
+    filebrowserImageUploadUrl: '$kcPath/upload.php?type=images',
+    filebrowserFlashUploadUrl: '$kcPath/upload.php?type=flash'
+});
+</script>
+END;
+        return $html;
+    }
+}
